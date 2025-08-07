e5f4124da7fbea1102db50ae4206081a885b592f
msr <msrxse@gmail.com>
2025-08-07 18:57:37 +0100

    Add loading state to form submit

---
+++ src/components/FormSubmitButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmitButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>,
+) {
+  // This hooks reads the status of the parent form
+  const { pending } = useFormStatus();
+
+  return (
+    <Button {...props} type="submit" disabled={props.disabled || pending}>
+      <span className="flex items-center justify-center gap-1">
+        {pending && <Loader2 size={16} className="animate-spin" />}
+        {props.children}
+      </span>
+    </Button>
+  );
+}
--- src/components/JobFilterSidebar.tsx
+++ src/components/JobFilterSidebar.tsx
@@ -1,3 +1,4 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -112,9 +113,7 @@ export default async function JobFilterSidebar({
             />
             <Label htmlFor="remote">Remote jobs</Label>
           </div>
-          <Button type="submit" className="w-full">
-            Filter jobs
-          </Button>
+          <FormSubmitButton className="w-full">Filter jobs</FormSubmitButton>
         </div>
       </form>
     </aside>
