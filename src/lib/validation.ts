196f50df64c6f93c7f3ed05d182cd45fd3fc41a3
msr <msrxse@gmail.com>
2025-08-11 21:21:28 +0100

    Add validation for the create job form

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,61 @@
+import { jobTypes, locationTypes } from "@/lib/job-types";
 import z from "zod";
+
+const requiredString = z.string().min(1, "Required");
+const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
+
+const companyLogoSchema = z
+  .custom<File | undefined>()
+  .refine(
+    (file) => !file || (file instanceof File && file.type.startsWith("image/")),
+    "Must be an image file",
+  )
+  .refine(
+    (file) => !file || file.size < 1024 * 1024 * 2,
+    "File must be less than 2MB",
+  );
+
+const applicationSchema = z
+  .object({
+    applicationEmail: z.string().max(100).email().optional().or(z.literal("")), // OR necessary for optional together with email
+    applicationUrl: z.string().max(100).url().optional().or(z.literal("")),
+  })
+  .refine((data) => data.applicationEmail || data.applicationUrl, {
+    message: "Email or url is required",
+    path: ["application_email"],
+  });
+
+const locationsSchema = z
+  .object({
+    locationType: requiredString.refine(
+      (value) => locationTypes.includes(value),
+      "Invalid location type",
+    ),
+    location: z.string().max(100).optional(),
+  })
+  .refine(
+    (data) =>
+      !data.locationType || data.locationType === "Remote" || data.location,
+    {
+      message: "Location is required for on-site jobs",
+      path: ["location"],
+    },
+  );
+
+export const createJobSchema = z
+  .object({
+    title: requiredString.max(100),
+    type: requiredString.refine(
+      (value) => jobTypes.includes(value),
+      "Invalid job type",
+    ),
+    companyName: requiredString.max(100),
+    companyLogo: companyLogoSchema,
+    description: z.string().max(500).optional(),
+    salary: numericRequiredString.max(9, "Number can't be more that 9 digits"), // will fit into a int (less than a million)
+  })
+  .and(applicationSchema)
+  .and(locationsSchema);
 
 export const jobFilterSchema = z.object({
   q: z.string().optional(),
@@ -8,3 +65,4 @@ export const jobFilterSchema = z.object({
 });
 
 export type JobFilterValues = z.infer<typeof jobFilterSchema>;
+export type CreateJobValues = z.infer<typeof createJobSchema>;
